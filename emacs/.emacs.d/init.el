f37c9a071abf9aea043286b30753298acd2db203
Lucas Sahar <lucas.sahar@mail.mcgill.ca>
2020-05-01 00:59:15 -0400

    initialize dotfile respository

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,199 @@
+(menu-bar-mode -1)
+
+
+
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+  ;; and `package-pinned-packages`. Most users will not need or want to do this.
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  )
+(package-initialize)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(custom-enabled-themes (quote (deeper-blue))) ;will override ewal, normally
+ '(inhibit-startup-screen t)
+ '(menu-bar-mode nil)
+ '(package-selected-packages
+   (quote
+    (use-package ;; ewal
+      cider right-click-context markdown-mode tuareg)))
+ '(tool-bar-mode nil)
+ '(xterm-mouse-mode t))
+
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+(setq vc-follow-symlinks t)
+
+;;enable right-click menu
+(right-click-context-mode 1)
+
+;;update files automatically when changed on disk
+(global-auto-revert-mode t)
+
+
+
+;;compile with pdflatex by default
+(setq latex-run-command "pdflatex")
+
+
+
+
+;<works>
+;; (use-package ewal
+;;   :init (setq ewal-use-built-in-always-p nil
+;;               ewal-use-built-in-on-failure-p t
+;;               ewal-built-in-palette "sexy-material"))
+;</works>
+
+
+;; (use-package ewal-spacemacs-themes     
+;;   :init (progn
+;;           (setq spacemacs-theme-underline-parens t
+;;                 my:rice:font (font-spec
+;;                               :family "Source Code Pro"
+;;                               :weight 'semi-bold
+;;                               :size 11.0))
+;;           (show-paren-mode +1)
+;;           (global-hl-line-mode)
+;;           (set-frame-font my:rice:font nil t)
+;;           (add-to-list  'default-frame-alist
+;;                         `(font . ,(font-xlfd-name my:rice:font))))
+;;   :config (progn
+;;             (load-theme 'ewal-spacemacs-modern t)
+;;             (enable-theme 'ewal-spacemacs-modern)))
+;; (use-package ewal-evil-cursors
+;;   :after (ewal-spacemacs-themes)
+;;   :config (ewal-evil-cursors-get-colors
+;;            :apply t :spaceline t))
+;; (use-package spaceline
+;;   :after (ewal-evil-cursors winum)
+;;   :init (setq powerline-default-separator nil)
+;;   :config (spaceline-spacemacs-theme))
+
+
+
+; key bindings: wish me luck!
+
+
+
+;(define-key global-map "\C-z" 'undo)
+;(define-key global-map "\C-Z" 'redo)
+;(define-key global-map "\C-/" 'isearch-forward)
+;(define-key global-map "\C-?" 'isearch-backward)
+
+;for muscle memory
+(global-unset-key "\C-n")
+(global-unset-key "\C-p")
+(global-unset-key "\M-f")
+(global-unset-key "\M-b")
+(global-unset-key "\C-f")
+(global-unset-key "\C-b")
+(global-unset-key "\C-a")
+(global-unset-key "\C-e")
+(global-unset-key "\M-a")
+(global-unset-key "\M-v")
+(global-unset-key "\C-v")
+(global-unset-key "\M-s")
+;(global-unset-key "\C-x\C-s")
+;(global-unset-key "\C-xu")
+;(global-unset-key "\C-xk")
+
+
+;<known to work>
+
+
+
+(define-key global-map "\C-f" 'isearch-forward)
+(define-key global-map "\M-f" 'isearch-backward)
+(define-key isearch-mode-map "\C-f" 'isearch-repeat-forward)
+(define-key isearch-mode-map "\M-f" 'isearch-repeat-backward)
+
+
+; text movement
+(define-key global-map "\M-k" 'move-end-of-line)
+(define-key global-map "\M-j" 'move-beginning-of-line)
+(define-key global-map "\C-l" 'forward-char)
+(define-key global-map "\C-h" 'backward-char)
+
+(define-key global-map "\C-j" 'next-line)
+(define-key global-map "\C-k" 'previous-line)
+(define-key global-map "\M-l" 'forward-word)
+(define-key global-map "\M-h" 'backward-word)
+
+
+
+(define-key global-map "\M-g" 'beginning-of-buffer)
+(define-key global-map "\M-G" 'end-of-buffer)
+
+
+
+
+
+;text manipulation
+
+(define-key global-map "\C-a" 'mark-whole-buffer)
+
+
+(define-key global-map "\M-x" 'kill-region)
+(define-key global-map "\M-c" 'kill-ring-save)
+(define-key global-map "\M-v" 'yank)
+
+
+(define-key global-map "\C-d" 'delete-char)
+(define-key global-map "\M-d" 'kill-line)
+(define-key global-map "\M-w" 'kill-word)
+(define-key global-map "\C-w" 'kill-word)
+
+
+;general operations & IO
+(define-key global-map "\C-b" 'switch-to-buffer)
+(define-key global-map "\C-e" 'eval-last-sexp)
+
+(define-key global-map "\C-s" 'save-buffer)
+(define-key global-map "\M-s" 'save-buffer)
+
+; windows
+(define-key global-map (kbd "<C-tab>") 'other-window)
+
+;testing
+(define-key global-map "\M-e" 'execute-extended-command)
+
+(define-key global-map "\C-q" 'keyboard-quit)
+
+
+
+;</known to work>
+
+
+
+
+;; paragraph?			
+;; backward paragraph?
+
+
---
+++ zsh/.zshrc
@@ -0,0 +1,144 @@
+# The following lines were added by compinstall
+
+zstyle ':completion:*' completer _expand _complete _ignored _correct _approximate
+zstyle ':completion:*' completions 1
+zstyle ':completion:*' glob 1
+zstyle ':completion:*' list-colors ''
+zstyle ':completion:*' list-prompt %SAt %p: %s
+zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} r:|[.-_]=** r:|=**' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} r:|[._-]=** r:|=** l:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} r:|[._-]=** r:|=** l:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} r:|[._-]=** r:|=** l:|=*'
+zstyle ':completion:*' max-errors 1
+zstyle ':completion:*' menu select=1
+zstyle ':completion:*' prompt '%e error(s) found. Select a replacement:'
+zstyle ':completion:*' select-prompt %SScrolling active: %p%s
+zstyle ':completion:*' substitute 1
+zstyle :compinstall filename '/home/l-acs/.zshrc'
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.config/shell/histfile
+HISTSIZE=1000000
+SAVEHIST=10000000000
+setopt appendhistory autocd
+unsetopt beep nomatch notify
+
+# End of lines configured by zsh-newuser-install
+
+#my configurations
+bindkey -e
+source ~/.config/shell/zshbindings
+setopt INTERACTIVE_COMMENTS
+
+
+#environment variables
+export XDG_CONFIG_HOME="$HOME/.config"
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:$HOME/.scripts:$HOME/.scripts/window"
+
+export EDITOR="emacsclient -t"
+#"/usr/bin/emacs -nw"
+export VISUAL="emacsclient -create-frame --alternate-editor=''"
+#"emacs"
+export TERMINAL="urxvt -e tmux"
+
+export GTK_THEME="Adwaita:dark"
+export URXVT_PERL_LIB="$HOME/.config/urxvt/perl/"
+
+#for scripts
+export JOT_DIR="$HOME/Documents/Notes"
+export TD_DIR="$HOME/Documents/Days"
+export EMAIL="thunderbird"
+export FILEBROWSER="browse"
+export NINEFLAGS="-bg #fdf6e3 -fg #222222 -font 'UbuntuMono-R' -popup -teleport" #6A6B6D 181A26
+export MUSIC="$HOME/Music"
+export VIDEO="$HOME/Documents/Videos"
+export WEB="firefox"
+
+
+
+#aliases & functions
+
+#abbreviations:
+alias e="emacsclient -t"
+alias blather="$HOME/.programs/blather/Blather.py"
+alias m="ncmpcpp"
+z(){
+    zathura "${@}" & disown
+}
+#alias e="emacs"
+
+#global
+alias -g xs='$(xsel -o)'
+
+
+
+#files, etc
+alias zrc="$EDITOR ~/.zshrc"
+alias token="cat $JOT_DIR/ocaml_token.txt | xsel -bi"
+alias sepia="cat $JOT_DIR/sepia.txt | xsel -bi"
+alias cover="feh --scale-down -g 599x377 --image-bg black --reload=1 .scripts/output/cover.jpg"
+
+
+
+
+
+#utils
+alias reload="source ~/.zshrc"
+cl(){
+    cd "${@}" && ls --color
+}
+
+space(){ #only a function because of quoting nightmares
+	df -h | grep sda4 | awk '{print $3 " of " $2 " (" $5 ") used. " $4 " remaining."}'
+}
+
+
+#configurations
+alias dash="rlwrap dash"
+
+emacs(){
+    #/usr/bin/emacs "${@}"  & disown
+    emacsclient -create-frame --alternate-editor="" "${@}"
+}
+alias ls="ls --color"
+alias ocaml="rlwrap ocaml"
+
+
+#corrections
+alias clear="echo 'Try Ctrl+L'"
+alias vim="echo 'Try "e"'"
+#"emacs -nw"
+
+
+
+
+#memes etc
+alias lofi="mpv 'https://www.youtube.com/watch?v=5qap5aO4i9A'"
+
+
+
+
+
+
+alias mdl='youtube-dl -i -f bestaudio\[ext=m4a\] --embed-thumbnail -o "~/Music/%(title)s.%(ext)s"'
+alias vdl-sub='youtube-dl -i -f worst -o "~/Documents/Videos/yt/%(uploader)s/%(upload_date)s %(title)s.%(ext)s" --write-auto-sub'
+alias vdl='youtube-dl -i -f worst -o "~/Documents/Videos/yt/%(uploader)s/%(upload_date)s %(title)s.%(ext)s"'
+
+
+
+
+#
+#alias tlmgr='/usr/share/texmf-dist/scripts/texlive/tlmgr.pl --usermode'
+
+
+
+
+
+#zsh enhancements
+#----
+
+#colours!
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+#history: autosuggestions
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
