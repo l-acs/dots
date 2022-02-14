916f573ce59a4b60c017e2e970c105d981fb5887
l-acs <lucas.sahar@mail.mcgill.ca>
2022-02-13 22:54:18 -0500

    Stop auto-moving subsequent Firefoxes

--- bspwm/.config/bspwm/ext.sh
+++ bspwm/.config/bspwm/ext.sh
@@ -26,15 +26,3 @@ if [ "$instance" = zoom ] ; then
 	    ;;
     esac
 fi
-
-case "$class" in
-    "firefox")
-
-	if [ $(pgrep -f 'firefox .*-childID.*' | wc -l) -gt 1 ]; then # there's already a firefox open
-	    [ $monitor_count -ge 1 ] && [ ! $second_monitor_occupied ]  && echo 'monitor=^2 follow=on focus=on'
-	else
-	    echo 'desktop=^5' # this is the first firefox
-	fi
-
-	;;
-esac
